440fc62233eec8e9d7648a2c88908c9d2f852026
Mirik9724 <145905797+Mirik9724@users.noreply.github.com>
2024-05-22 14:50:51 +1000

    Delete lbs_install_all_libs_for_windows.bat

--- lbs_install_all_libs_for_windows.bat
+++
@@ -1,8 +0,0 @@
-pip install tkinter
-pip install minecraft-launcher-lib
-pip install subprocess
-pip install random-username
-pip install uuid
-pip install os
-pip install threading
-pip install pillow
